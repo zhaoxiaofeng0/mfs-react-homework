a270d725cee7d4994a2cb86413e4d1c5a75f2fb1
zhaoxiaofeng0 <772380291@qq.com>
2018-05-30 16:53:50 +0800

    代码题

--- Es6-two/demo7.js
+++ Es6-two/demo7.js
@@ -1,2 +1,11 @@
 //请将 fs.readFile(path[, options], callback) 手动函数 Thunk 化，了解函数点击这里
-asd
+const thunkify  = function (fn){
+    return function(...args){
+        return function(callback){
+            return fn.call(this, ...args,callback);
+        }
+    }
+}
+
+var readFileThunk = thunkify(fs.readFile);
+readFileThunk("filename")(callback);
